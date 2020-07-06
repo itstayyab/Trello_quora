ee233572709bf57d3aca7221a4d78ee2bb763e95
sushmak49 <sushmak49@gmail.com>
2020-07-06 17:36:20 +0530

    Code refactor,user-authorization methods added

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
@@ -1,13 +1,19 @@
 package com.upgrad.quora.service.business;
 
 import com.upgrad.quora.service.dao.QuestionDao;
+import com.upgrad.quora.service.dao.UserAuthDao;
 import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.InvalidQuestionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class QuestionService {
@@ -15,15 +21,91 @@ public class QuestionService {
     @Autowired
     private QuestionDao questionDao;
 
-    //Create a new question entity
+    @Autowired
+    private UserAuthDao userAuthDao;
+
+    //Authorize user who wants to post a question
+    public UserAuthEntity createQuestionAuthorization(final String authorization) throws AuthorizationFailedException {
+        UserAuthEntity userAuthEntity = userAuthDao.getUserAuthByToken(authorization);
+        if (userAuthEntity==null) {
+            throw new AuthorizationFailedException("ATHR-001","User has not signed in");
+        } else {
+            //Retrieve logout_at attribute value of UserAuthEntity to check if user has already signed out
+            ZonedDateTime logoutAt = userAuthEntity.getLogoutAt();
+            if (logoutAt!=null) {
+                throw new AuthorizationFailedException("ATHR-002","User is signed out.Sign in first to post a question");
+            } else {
+                return userAuthEntity;
+            }
+        }
+    }
+
+    //Create a new Question entity
     @Transactional(propagation = Propagation.REQUIRED)
     public QuestionEntity createNewQuestion(QuestionEntity questionEntity) {
+        // Assign a UUID to the question that is being created.
+        questionEntity.setUuid(UUID.randomUUID().toString());
         return questionDao.createQuestion(questionEntity);
     }
 
-    //Get a list of all question entities
+    //Authorize user who wants to get a list of all questions
+    public UserAuthEntity getAllQuestionsAuthorization(final String authorization) throws AuthorizationFailedException {
+        UserAuthEntity userAuthEntity = userAuthDao.getUserAuthByToken(authorization);
+        if (userAuthEntity==null) {
+            throw new AuthorizationFailedException("ATHR-001","User has not signed in");
+        } else {
+            //Retrieve logout_at attribute value of UserAuthEntity to check if user has already signed out
+            ZonedDateTime logoutAt = userAuthEntity.getLogoutAt();
+            if (logoutAt!=null) {
+                throw new AuthorizationFailedException("ATHR-002","User is signed out.Sign in first to get all questions");
+            } else {
+                return userAuthEntity;
+            }
+        }
+    }
+
+    //Get a list of all Question entities
     public List<QuestionEntity> getAllQuestions() {
         return questionDao.getAllQuestions();
     }
 
+    //Get Question entity by question Uuid
+    public QuestionEntity getQuestionByUuid(String questionUuid) throws InvalidQuestionException {
+        QuestionEntity questionByUuid = questionDao.getQuestionByUuid(questionUuid);
+        if (questionByUuid==null) {
+           throw new InvalidQuestionException("QUES-001","Entered question uuid does not exist");
+        }
+        return questionByUuid;
+    }
+
+    //Validate whether the question owner and editor are same users
+    public boolean authorizeQuestionOwner(Integer questionEditorId, Integer questionOwnerId) throws AuthorizationFailedException {
+        if (!questionEditorId.equals(questionOwnerId)) {
+            throw new AuthorizationFailedException("ATHR-003","Only the question owner can edit the question");
+        } else {
+            return true;
+        }
+    }
+
+    //Edit question content
+    @Transactional(propagation = Propagation.REQUIRED)
+    public QuestionEntity editQuestion(QuestionEntity questionEntity) {
+        return questionDao.editQuestion(questionEntity);
+    }
+
+    //Check whether user is authorized to edit question
+    public UserAuthEntity editQuestionAuthorization(final String authorization) throws AuthorizationFailedException {
+        UserAuthEntity userAuthEntity = userAuthDao.getUserAuthByToken(authorization);
+        if (userAuthEntity==null) {
+            throw new AuthorizationFailedException("ATHR-001","User has not signed in");
+        } else {
+            //Retrieve logout_at attribute value of UserAuthEntity to check if user has already signed out
+            ZonedDateTime logoutAt = userAuthEntity.getLogoutAt();
+            if (logoutAt!=null) {
+                throw new AuthorizationFailedException("ATHR-002","User is signed out.Sign in first to edit the question");
+            } else {
+                return userAuthEntity;
+            }
+        }
+    }
 }
