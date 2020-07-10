876c4888419f4097b96467d65f561d5c4c93302b
sushmak49 <sushmak49@gmail.com>
2020-07-10 23:39:19 +0530

    Format code in Controller classes

--- quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
@@ -21,13 +21,14 @@ public class CommonController {
 
     /**
      * Controller method that serves userProfile GET endpoint
+     *
      * @param userUuid
      * @param authorization
      * @return User profile of a user
      * @throws AuthorizationFailedException
      * @throws UserNotFoundException
      */
-    @RequestMapping(method = RequestMethod.GET, path ="/userprofile/{userId}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(method = RequestMethod.GET, path = "/userprofile/{userId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<UserDetailsResponse> getUserProfile(@PathVariable("userId") final String userUuid, @RequestHeader("authorization") final String authorization) throws AuthorizationFailedException, UserNotFoundException {
 
         //Check if user has signed-in or signed-out already by validating the access-token
--- quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -25,6 +25,7 @@ public class QuestionController {
 
     /**
      * Controller method to handle createQuestion POST endpoint
+     *
      * @param questionRequest
      * @param authorization
      * @return QuestionResponse
@@ -32,7 +33,7 @@ public class QuestionController {
      */
     @RequestMapping(method = RequestMethod.POST, path = "/question/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<QuestionResponse> createQuestion(QuestionRequest questionRequest, @RequestHeader("authorization") final String authorization) throws AuthorizationFailedException {
-       //Create new Question Entity
+        //Create new Question Entity
         QuestionEntity questionEntity = new QuestionEntity();
         questionEntity.setContent(questionRequest.getContent());
         questionEntity.setDate(ZonedDateTime.now());
@@ -47,6 +48,7 @@ public class QuestionController {
 
     /**
      * Controller method to handle GET request to fetch all questions
+     *
      * @param authorization
      * @return List of all questions
      * @throws AuthorizationFailedException
@@ -60,16 +62,17 @@ public class QuestionController {
         final List<QuestionDetailsResponse> questionResponseList = new ArrayList<>();
 
         //Extract Uuid and content from each QuestionResponse entity
-        for(QuestionEntity question:allQuestions) {
+        for (QuestionEntity question : allQuestions) {
             String uuid = question.getUuid();
             String content = question.getContent();
             questionResponseList.add(new QuestionDetailsResponse().id(uuid).content(content));
         }
-        return new ResponseEntity<List<QuestionDetailsResponse>>(questionResponseList,HttpStatus.OK);
+        return new ResponseEntity<List<QuestionDetailsResponse>>(questionResponseList, HttpStatus.OK);
     }
 
     /**
-     *Controller method to handle PUT request to update question
+     * Controller method to handle PUT request to update question
+     *
      * @param questionEditRequest
      * @param questionUuid
      * @param authorization
@@ -87,19 +90,20 @@ public class QuestionController {
 
         //Set the Uuid and status of edited question in response
         QuestionEditResponse questionEditResponse = new QuestionEditResponse().id(questionEntity.getUuid()).status("QUESTION EDITED");
-        return new ResponseEntity<QuestionEditResponse>(questionEditResponse,HttpStatus.OK);
+        return new ResponseEntity<QuestionEditResponse>(questionEditResponse, HttpStatus.OK);
     }
 
     /**
      * Controller method to get all questions posted by a user
+     *
      * @param accessToken
      * @param userId
      * @return
      * @throws AuthorizationFailedException
      * @throws UserNotFoundException
      */
-    @RequestMapping(method = RequestMethod.GET,path = "question/all/{userId}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<List<QuestionDetailsResponse>> getQuestionByUserId(@RequestHeader("authorization") final String accessToken,@PathVariable("userId") String userId)
+    @RequestMapping(method = RequestMethod.GET, path = "question/all/{userId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<QuestionDetailsResponse>> getQuestionByUserId(@RequestHeader("authorization") final String accessToken, @PathVariable("userId") String userId)
             throws AuthorizationFailedException, UserNotFoundException {
         List<QuestionEntity> questions = questionService.getAllQuestionsByUser(userId, accessToken);
         List<QuestionDetailsResponse> questionDetailResponses = new ArrayList<>();
@@ -111,6 +115,16 @@ public class QuestionController {
         }
         return new ResponseEntity<List<QuestionDetailsResponse>>(questionDetailResponses, HttpStatus.OK);
     }
+
+    /**
+     * Controller method to handle DELETE request to delete question
+     *
+     * @param accessToken
+     * @param questionId
+     * @return
+     * @throws AuthorizationFailedException
+     * @throws InvalidQuestionException
+     */
     @RequestMapping(method = RequestMethod.DELETE, path = "/question/delete/{questionId}")
     public ResponseEntity<QuestionDeleteResponse> deleteQuestion(
             @RequestHeader("authorization") final String accessToken,
